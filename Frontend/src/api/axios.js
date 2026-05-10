224be0903c7da626be886812fb0292c1bbb6e939
TadesenaVaishnavi <160591205+TadesenaVaishnavi@users.noreply.github.com>
2026-05-10 10:28:28 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,15 +1,29 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://localhost:9090",
+const AUTH_API = axios.create({
+  baseURL: "https://ctube-correction-4.onrender.com",
 });
 
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+export const loginUser = (data) => {
+  return AUTH_API.post("/auth/login", data);
+};
 
-export default instance;
+export const registerUser = (data) => {
+  return AUTH_API.post("/auth/register", data);
+};
+
+// import axios from "axios";
+
+// const instance = axios.create({
+//   baseURL: "http://localhost:9090",
+// });
+
+// instance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default instance;
